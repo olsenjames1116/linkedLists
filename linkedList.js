ddac5069871e6fad83b22b15ce88c8c501f16dac
James Olsen <olsenjames1116@gmail.com>
2023-02-04 12:47:29 -0700

    Add commenting

--- linkedList.js
+++ linkedList.js
@@ -5,6 +5,7 @@ class LinkedList {
         this.listHead = new Node();
     }
 
+    // Return and set the first Node in the list.
     get head() {
         return this.listHead.next;
     }
@@ -13,11 +14,14 @@ class LinkedList {
         this.listHead.next = value;
     }
 
+    // Return and set the last Node in the list.
     get tail() {
+        // Returns null if there are no Nodes in the list.
         if(this.head === null) return this.listHead;
 
         let temp = this.head;
 
+        // The last Node is reached when the one following it is null.
         while(temp.next !== null) {
             temp = temp.next;
         }
@@ -29,12 +33,15 @@ class LinkedList {
         this.tail.next = value;
     }
 
+    // Return how many Nodes are in the list.
     get size() {
+        // Returns 0 if there are no Nodes in the list.
         if(this.head === null ) return 0;
 
         let temp = this.head;
         let length;
 
+        // The last Node is reached when the one following it is null.
         for(length = 1; temp.next !== null; length++) {
             temp = temp.next;
         }
@@ -42,25 +49,34 @@ class LinkedList {
         return length;
     }
 
+    // Push a new Node to the end of the list.
     append(value) {
         const node = new Node(value);
 
+        /* If there are not any Nodes in the list yet, the first Node is now the appended Node. Otherwise, the final 
+        Node is now the appended Node. */
         this.head === null ? this.head = node : this.tail = node;
     }
 
+    // Add a new node to the front of the list.
     prepend(value) {
+        // If there are not any Nodes in the list yet, the first Node is not the prepended Node.
         if(this.head === null ) {
             this.head = new Node(value);
         } else {
+            /* There are Nodes in the list, so the first Node now becomes the prepended Node and the pointer is set
+            to the previous first Node's pointer. */
             const firstNode = this.head;
             this.head = new Node(value, firstNode);
         }
     }
 
+    // Convert the list to a visual representation.
     toString() {
         let temp = this.head;
         let string = ``;
 
+        // The last Node is reached when the one following it is null.
         while(temp !== null) {
             string += `( ${temp.data} ) -> `;
             temp = temp.next;
@@ -69,11 +85,14 @@ class LinkedList {
         return string += `null`;
     }
 
+    // Determines which Node is at the specified index.
     at(index) {
+        // If there is an error on entering an index that does not exist in the list, it is handled here.
         if (index >= this.size || index < 0) return undefined;
 
         let temp = this.listHead.next;
 
+        // Continues through Node pointers until it reaches the correct index.
         for(let i = 0; i < index; i++) {
             temp = temp.next;
         }
@@ -81,20 +100,25 @@ class LinkedList {
         return temp;
     }
 
+    // Remove and return the last Node in the list.
     pop() {
         if(this.size === 1) {
+            // If the list only contains one Node, it returns that node and clears the list.
             const removedNode = this.head;
             this.listHead.next = null;
             return removedNode;
         }
 
         if(this.head !== null) {
+            // If the head is not null, there are Nodes in the list.
             let temp = this.head;
 
+            // The last node is reached when the one following it is null
             while(temp.next.next !== null) {
                 temp = temp.next;
             }
 
+            // Remove the Node by removing the pointer that points to the Node.
             const removedNode = temp.next;
             temp.next = null;
             return removedNode;
@@ -103,28 +127,37 @@ class LinkedList {
         return undefined;
     }
 
+    // Returns a boolean if the specified value is in the list.
     contains(value) {
+        // If the first Node is null, there are not any Nodes in the list.
         if(this.head === null) return false;
 
         let temp = this.head;
 
+        // Continues through the list until it reaches the end.
         for(let i = 0; i < this.size; i++) {
 
             if(temp.data === value) {
+                // Reached if the value is found in the list.
                 return true;
             }
 
             temp = temp.next;
         }
 
+        // Reached if the iterator makes it through the list without finding the value.
         return false;
     }
 
+    // Returns the index of the specified value.
     find(value) {
+        // The list does not contain the value, so there is not an associated index.
         if(!this.contains(value)) return null;
 
+        // Reached if the list is known to contain the specified value.
         let temp = this.head;
 
+        // Continues through the list until it reaches the end.
         for(let index = 0; index < this.size; index++) {
 
             if(temp.data === value) {
@@ -135,9 +168,12 @@ class LinkedList {
         }
     }
 
+    // Inserts the value at the specified index.
     insertAt(value, index) {
+        // If there is an error on entering an index that does not exist in the list, it is handled here.
         if(index >= this.size || index < 0) return undefined;
 
+        // If the index is zero, this is the same process as prepending the value.
         if(index === 0) {
             this.prepend(value);
             return;
@@ -146,17 +182,23 @@ class LinkedList {
         let temp = this.head;
         let previousNode;
 
+        // Continues through the list until it reaches the specified index.
         for(let i = 0; i < index; i++) {
             previousNode = temp;
             temp = temp.next;
         }
 
+        /* To insert the value, the value that pointed to the previous Node at that index must now point to the new
+        Node. */
         previousNode.next = new Node(value, temp);
     }
 
+    // Removes the value at the specified index.
     removeAt(index) {
+        // If there is an error on entering an index that does not exist in the list, it is handled here.
         if(index >= this.size || index < 0) return undefined;
 
+        // If the index is 0, the head Node is replaced by the next Node in the list.
         if(index === 0) {
             this.listHead.next = this.head.next;
         }
@@ -164,15 +206,18 @@ class LinkedList {
         let temp = this.head;
         let previousNode;
 
+        // Continues through the list until it reaches the specified index.
         for(let i = 0; i < index; i++) {
             previousNode = temp;
             temp = temp.next;
         }
 
+        // To remove a Node, the previous Node must point to the Node after the removed Node.
         previousNode.next = temp.next;
     }
 }
 
+// Everything below here is testing of the created methods.
 const linkedList = new LinkedList();
 
 linkedList.append(1);
